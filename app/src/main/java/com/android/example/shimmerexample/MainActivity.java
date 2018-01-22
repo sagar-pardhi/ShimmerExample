d8d6aab97ed62c69addd95b3a1ca4ed12f209c29
sagar-pardhi <sgrpardhi007@gmail.com>
2018-01-22 21:00:13 +0530

    Added method to fetch recipe list from json in ActivityMain and added ItemDecoration.java file

--- app/src/main/java/com/android/example/shimmerexample/MainActivity.java
+++ app/src/main/java/com/android/example/shimmerexample/MainActivity.java
@@ -4,16 +4,41 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.facebook.shimmer.ShimmerFrameLayout;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private RecyclerView recyclerView;
+    private List<Recipe> cartList;
+    private RecipeListAdapter mAdapter;
+
     private ShimmerFrameLayout mShimmerViewContainer;
+
+    // URL to fetch menu json
+    // this endpoint takes 2 sec before giving the response to add
+    // some delay to test the Shimmer effect
+    private static final String URL = "https://api.androidhive.info/json/shimmer/menu.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +46,60 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mShimmerViewContainer = findViewById(R.id.shimmer_view_container);
+
+        mShimmerViewContainer = findViewById(R.id.shimmer_view_container);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        cartList = new ArrayList<>();
+        mAdapter = new RecipeListAdapter(this, cartList);
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.addItemDecoration(new MyDividerItemDecoration(this, LinearLayoutManager.VERTICAL, 16));
+        recyclerView.setAdapter(mAdapter);
+
+        // making http call and fetching menu json
+        fetchRecipes();
+    }
+
+    /**
+     * method make volley network call and parses json
+     */
+    private void fetchRecipes() {
+        JsonArrayRequest request = new JsonArrayRequest(URL,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        if (response == null) {
+                            Toast.makeText(getApplicationContext(), "Couldn't fetch the menu! Pleas try again.", Toast.LENGTH_LONG).show();
+                            return;
+                        }
+
+                        List<Recipe> recipes = new Gson().fromJson(response.toString(), new TypeToken<List<Recipe>>() {
+                        }.getType());
+
+                        // adding recipes to cart list
+                        cartList.clear();
+                        cartList.addAll(recipes);
+
+                        // refreshing recycler view
+                        mAdapter.notifyDataSetChanged();
+
+                        // stop animating Shimmer and hide the layout
+                        mShimmerViewContainer.stopShimmerAnimation();
+                        mShimmerViewContainer.setVisibility(View.GONE);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // error in getting json
+                Log.e(TAG, "Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(), "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        MyApplication.getInstance().addToRequestQueue(request);
     }
 
     @Override
