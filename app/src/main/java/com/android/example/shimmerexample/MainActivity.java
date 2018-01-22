3a857df8ba442e88f2fb109787f3b0842592b6c5
sagar-pardhi <sgrpardhi007@gmail.com>
2018-01-22 20:14:57 +0530

    Added ShimmerFrameLayout in activity_main.xml and MainActivity

--- app/src/main/java/com/android/example/shimmerexample/MainActivity.java
+++ app/src/main/java/com/android/example/shimmerexample/MainActivity.java
@@ -9,23 +9,30 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.facebook.shimmer.ShimmerFrameLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ShimmerFrameLayout mShimmerViewContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mShimmerViewContainer = findViewById(R.id.shimmer_view_container);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mShimmerViewContainer.startShimmerAnimation();
+    }
+
+    @Override
+    protected void onPause() {
+        mShimmerViewContainer.stopShimmerAnimation();
+        super.onPause();
     }
 
     @Override
